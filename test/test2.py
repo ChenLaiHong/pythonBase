19562342d61352f813d5ee700518740154c70890
ChenLaiHong <1185630400@qq.com>
2018-04-19 08:33:57 +0800

    python基本运算符

---
+++ test/test2.py
@@ -0,0 +1,30 @@
+
+#语句input,输入内容
+# x = input("x:")
+# y = input("y:")
+# print(x)
+# print(y)
+#相当于字符串拼接
+# print(x+y)
+#两个整型相加
+# print(int(x)+int(y))
+
+
+#语句if
+# if 1==2 : print("1等于2")
+# if 1==1 : print("1等于1")
+
+#pow求幂
+# print(2**3)
+import math
+
+print(math.pow(2,3))
+
+#求绝对值
+print(abs(-10))
+
+#向下圆整
+# print(2//3)
+#取最接近的整数
+print(round(2/3))
+print(round(2/-3))
