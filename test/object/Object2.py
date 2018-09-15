8b1d391ff71735b488a696f1d435fe61567f6459
ChenLaiHong <1185630400@qq.com>
2018-09-15 13:40:47 +0800

    方法相关

--- test/object/Object2.py
+++ test/object/Object2.py
@@ -1,16 +1,31 @@
-# 类属性
+# 类属性,各个对象共享类属性
+# class Person:
+#     address = "浙江"
+#
+#
+# Person.name = "李四"
+# Person.age = 19
+#
+# print(Person.__dict__)
+#
+# # 注意：给类新增或者修改属性，不能通过对象去修改，只能是类名.属性
+# # 对象可以访问到类的属性
+# p = Person()
+# p.age = 25
+# print(p.name)
+# print(p.__dict__)
+# # 类与对象都有相同的属性就会先从对象中找
+# print(p.age)
+#
+# # 对象中的__dict__属性能直接被修改，类中的只能读
+# p.__dict__ = {"age": 36}
+# print(p.__dict__, p.age)
+
+# 在类里面限定对象能添加的属性
 class Person:
-    address = "浙江"
+    __slots__ = ["name", "age", "address", "phone"]
 
 
-Person.name = "李四"
-Person.age = 19
-
-print(Person.__dict__)
-
-# 对象可以访问到类的属性
 p = Person()
-p.age = 25
-print(p.name)
-# 类与对象都有相同的属性就会先从对象中找
+p.age = 18
 print(p.age)
---
+++ test/object/Object3.py
@@ -0,0 +1,33 @@
+# 方法相关，方法和函数调用方式不同
+# 方法调用要通过对象，函数直接就能调用
+# 定义函数
+def sum(i, j):
+    print(i + j)
+
+sum(1, 5)
+
+# 定义方法
+class Person:
+    # 实例方法默认第一个参数是一个实例，self是实例
+    def eat(self):
+        print("这是一个实例方法", self)
+
+    # 类方法默认第一个参数是一个类
+    @classmethod
+    def leifangfa(cls):
+        print("这是一个类方法", cls)
+
+    # 静态方法不作要求
+    @staticmethod
+    def jingtaifangfa():
+        print("这是一个静态方法")
+
+p = Person()
+p.eat()
+
+Person.jingtaifangfa()
+Person.leifangfa()
+p.jingtaifangfa()
+
+# 方法都是存放到类里面，存放到字典里
+print(Person.__dict__)
