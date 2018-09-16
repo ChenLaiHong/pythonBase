dfca8564e37262bbd2c1d6eb9a14a2ef7afe7e5c
ChenLaiHong <1185630400@qq.com>
2018-09-16 15:23:07 +0800

    变量的访问

--- test/object/PrivateFunction.py
+++ test/object/PrivateFunction.py
@@ -1,15 +1,23 @@
 # 方法的私有性
-# 什么都不添加代表公有
-# 在一个变量前面添加一个下划线代表受保护的属性
-# 两个下划线代表私有属性
+# 什么都不添加代表公有：
+# 类的内部、子类的内部、模块其他位置、跨模块都能正常访问
 
-# 测试公有变量
+# 在一个变量前面添加一个下划线代表受保护的属性
+# 类内部、子类内部正常访问；模块内其他位置、跨模块访问会有警告
+
+# 两个下划线代表私有属性
+# 类内部可以访问，子类内部、模块内其他位置、跨模块都不能访问
+
+# 测试公有变量、受保护变量
 # class Animal:
 #     x = 10
+#     _y = 233
 #     # 类的内部访问
 #     def test(self):
 #         print(Animal.x)
 #         print(self.x)
+#         print(Animal._y)
+#         print(self._y)
 #     pass
 #
 # class Dog(Animal):
@@ -18,6 +26,8 @@
 #     def test2(self):
 #         print(Dog.x)
 #         print(self.x)
+#         print(Dog._y)
+#         print(self._y)
 #     pass
 #
 # # 测试类的内部的方法
@@ -27,14 +37,52 @@
 # d = Dog()
 # d.test2()
 #
-# # 当前模块其他位置访问公有属性
+# # 当前模块其他位置访问
 # print(Animal.x)
 # print(Dog.x)
 #
+# print(Animal._y)
+# print(Dog._y)
+#
 # print(a.x)
 # print(d.x)
+#
+# print(a._y)
+# print(d._y)
 
-# 测试其他模块能不能访问公有变量a,在TestFunction.py模块访问x
+# # 测试其他模块能不能访问公有变量a,在TestFunction.py模块访问x
+__all__ = ["_y", "x", "__k"]
 x = 666
+_y = 233
+__k = 555
 
 
+# 私有属性
+
+# class Animal:
+#     __x = 100
+#     # 类的内部访问
+#     def test(self):
+#         print(Animal.__x)
+#         print(self.__x)
+#     pass
+#
+# class Dog(Animal):
+#     # 测试能不能继承这个私有变量
+#     def test2(self):
+#         print(Dog.__x)
+#         print(self.__x)
+#     pass
+#
+# # 测试类的内部的方法
+# a = Animal()
+# a.test()
+
+# d = Dog()
+# d.test2()
+
+# 当前模块其他位置访问
+# print(Animal.__x)
+# print(Dog.__x)
+# print(Animal.__x)
+# print(Dog.__x)
--- test/object/TestFunction.py
+++ test/object/TestFunction.py
@@ -1,6 +1,13 @@
 # import PrivateFunction
-#
 # print(PrivateFunction.x)
-
+# 有警告
+# print(PrivateFunction._y)
+# 可以访问
+# print(PrivateFunction.__k)
 from object.PrivateFunction import *
 print(x)
+# 有__all__访问会有警告
+# 没有__all__访问不了
+print(_y)
+
+print(__k)
