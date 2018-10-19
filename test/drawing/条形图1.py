7e8407a03f590659cece1f6851b4788f4278698d
ChenLaiHong <1185630400@qq.com>
2018-10-19 10:50:55 +0800

    条形图添加解析

--- test/drawing/条形图1.py
+++ test/drawing/条形图1.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
+# 设定x有值的地方
 x = [1, 3, 5, 7, 9]
+# 设定y有值的地方
 y = [4, 6, 2, 8, 9]
+# 画图
 plt.bar(x, y)
+# x的范围[0,12]，y的范围[0,10]
 plt.axis([0, 12, 0, 10])
+# 展示图
 plt.show()
