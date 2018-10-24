e4578f68ea183c74ce6457133d160aea351a68a4
ChenLaiHong <1185630400@qq.com>
2018-10-24 20:21:25 +0800

    。

--- test/drawing/作业条形图.py
+++ test/drawing/作业条形图.py
@@ -3,10 +3,15 @@ from sklearn.datasets import load_iris
 
 iris = load_iris()  # 加载机器学习的下的iris数据集,得到的是字典
 iris_data = iris["data"]
+# 存储sepal_length的数据
 sepal_length = []
+# 存储sepal_width的数据
 sepal_width = []
+# 存储petal_length的数据
 petal_length = []
+# 存储petal_width的数据
 petal_width = []
+# 循环150条数据，分别存起来
 for i in iris_data:
     sepal_length.append(i[0])
     sepal_width.append(i[1])
@@ -14,8 +19,8 @@ for i in iris_data:
     petal_width.append(i[3])
 # 为了能显示中文
 plt.rcParams['font.sans-serif'] = ['SimHei']
+# 这里是petal_width的条形图，其他的直接在这里修改就可以了
 plt.hist(petal_width, rwidth=0.9)
-
 plt.xlabel("花瓣宽度")
 plt.ylabel("计数")
 plt.show()
