498e130226add42d42518c76d81dba419270689a
ChenLaiHong <1185630400@qq.com>
2018-10-28 23:00:44 +0800

    盒状图

--- test/drawing/单个盒状图.py
+++ test/drawing/单个盒状图.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-names = ['sepal length', 'sepal width', 'petal length', 'petal width','class']
+names = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
 dataset = pd.read_csv(data, names=names)
 title = ["Iris", "Iris-setosa", "Iris-versicolor", "Iris-virginica"]
-dataList = [dataset, dataset[:50], dataset[50:100], dataset[100:150] ]
-for i in range(1,5):
+dataList = [dataset, dataset[:50], dataset[50:100], dataset[100:150]]
+for i in range(1, 5):
     plt.figure(i)
     dataList[i-1].plot(kind='box')
     plt.title(title[i-1])
