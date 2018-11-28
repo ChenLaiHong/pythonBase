e23022de7f6ef497a2dd9715f773d0f7aa35806c
ChenLaiHong <1185630400@qq.com>
2018-11-28 15:38:20 +0800

    8.2.17

---
+++ test/homework2/8.2.17-字符串裁剪.py
@@ -0,0 +1,19 @@
+"""
+【问题描述】编写函数delch(src, ch)，去掉字符串src中的字符ch，并返回新得到的字符串。
+【输入形式】一个字符串（不含空格）和要去掉的字符。字符串和字符中间有一个空格。
+【输出形式】去掉特定字符后的新字符串
+【样例输入】abdsag d
+【样例输出】absag
+"""
+def delch(src, ch):
+    while True:
+        num = src.find(ch)
+        if num != -1:
+            src = src.replace(ch, " ")
+        else:
+            break
+    return src.replace(" ","")
+
+
+src, ch = input().split()
+print(delch(src, ch))
