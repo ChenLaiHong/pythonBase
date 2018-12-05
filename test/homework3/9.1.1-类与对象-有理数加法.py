cf0b6b25999af9b327f02b0db896b733465f78ee
ChenLaiHong <1185630400@qq.com>
2018-12-05 23:19:08 +0800

    类与对象

---
+++ test/homework3/9.1.1-类与对象-有理数加法.py
@@ -0,0 +1,21 @@
+"""
+【问题描述】
+定义有理数类，定义计算两个有理数的和的方法。程序输入两个有理数，输出它们的和。
+【输入形式】
+输入在一行中按照a1/b1 a2/b2的格式给出两个分数形式的有理数，其中分子和分母全是正整数。
+【输出形式】
+在一行中按照a/b的格式输出两个有理数的和。注意必须是该有理数的最简分数形式，若分母为1，则只输出分子。
+【样例输入】
+1/3 1/6
+【样例输出】
+1/2
+"""
+class Shu:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def he(self, num1, num2):
+        return num1 + num2
+jisuan = Shu(1/3, 1/6)
+print(jisuan.he(jisuan.num1, jisuan.num2))
